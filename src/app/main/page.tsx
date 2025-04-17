96b91f83b756c284e4c73ca3ecbef190e7f98886
duyen26912 <nguyenduyen.260903@gmail.com>
2025-04-17 20:13:13 +0700

    Initial commit

---
+++ src/app/main/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold">Welcome to QAirline</h1>
+      <p>This is the homepage content</p>
+    </div>
+  );
+}
+
+ 
