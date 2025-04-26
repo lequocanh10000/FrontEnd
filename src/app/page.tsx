4b5da284713fd22096eda4b9cac3c857784bcb43
duyen26912 <nguyenduyen.260903@gmail.com>
2025-04-26 09:15:45 +0700

    new review

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Popular from "@/components/sections/Popular/popular";
 import Offer from "../components/sections/Offer/Offer";
 import About from "@/components/sections/About/about";
 import Blog from "@/components/sections/Blog/blog";
+import Review from "@/components/sections/Review/review";
 import JoinUs from "@/components/sections/JoinUs/joinUs";
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Offer />
       <About/>
       <Blog />
+      <Review/>
       <JoinUs/>
 
     </main>
